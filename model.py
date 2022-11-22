3e0741becd3fb141ec042148556e25f37c32b72a
Alevtin_linux_home <antonov3017@gmail.com>
2022-11-22 20:43:17 +0300

    some corrections

--- model.py
+++ model.py
@@ -2,17 +2,20 @@ from variables import *
 from csv import reader
 from prettytable import PrettyTable
 
+
 def show_child():
     try:
         read_file()
         print_table()
-
+    except IOError:
+        print('Файл не существует')
 
 
 def read_file(file_name=file_name):
     global lst_rows
     with open(file_name, 'r', encoding='utf-8') as file:
         lst_rows = list(reader(file, delimiter=';')
+
 
 def print_table():
     t = PrettyTable(field_names)
